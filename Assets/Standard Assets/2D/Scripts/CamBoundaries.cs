f8e960e8d12148c2e0d04fb639078824e1dfe828
Florian Benejean <florianbnj@gmail.com>
2018-11-19 22:24:18 +0100

    Smoothern the camera movments
    The camera will have a tuny lag following the player so that when is falls into the abyss he dies outside of the sight of the camera.

---
+++ Assets/Standard Assets/2D/Scripts/CamBoundaries.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBoundaries : MonoBehaviour {
+
+	public Vector2 minimumBoundary = new Vector2(2.0f, 6.0f);
+	public Vector2 maximumBoundary = new Vector2(10.0f, 12.0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	transform.position = new Vector3
+	(
+		Mathf.Clamp (transform.position.x, minimumBoundary.x, maximumBoundary.x),
+		Mathf.Clamp (transform.position.y, minimumBoundary.y, maximumBoundary.y),
+		transform.position.z
+	);
+
+	}
+}
